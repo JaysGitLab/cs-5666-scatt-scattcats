cca541c8cb195d7287deb9b79045804bbbf58baa
Erick <erick_lares@hotmail.com>
2017-04-22 16:02:32 -0400

    Map added no rework of the script class so far

---
+++ scratchgrader/CategoryMap.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package scratchgrader;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author erick
+ */
+public class CategoryMap{
+    
+    
+    private List<List<String>> categories = new ArrayList<List<String>>();
+    private static CategoryMap categoryMap = null;
+    
+    private void CategoryMap()
+    {   
+      
+         
+    }
+    
+    private void loadMap()
+    {
+      categories.add(createCategory("Control"));
+      categories.add(createCategory("Event"));
+      categories.add(createCategory("Looks"));
+      categories.add(createCategory("Motion"));
+      categories.add(createCategory("Operator"));
+      categories.add(createCategory("Pen"));
+      categories.add(createCategory("Sensing"));
+      categories.add(createCategory("Sound"));
+      categories.add(createCategory("DataVariables"));
+    }
+   
+    private List<String> createCategory(String category)
+    {    
+        List<String> blocks = new ArrayList<String>();
+        File file = new File("MapFolder/" + category);
+        ScratchGrader grader = new ScratchGrader(file.getAbsolutePath());
+        List<Sprite> sprites =  grader.getListOfSprites();
+        Object [] scripts = sprites.get(0).getScripts();
+    
+        for (int i = 0; i < scripts.length; i++)
+        {
+            blocks.add(scripts[i].toString());
+        }    
+        return blocks;
+    }
+
+    public static CategoryMap getInstance()
+    {
+        if(categoryMap == null){
+          categoryMap = new  CategoryMap();
+          categoryMap.loadMap();
+        }
+        
+        return categoryMap;
+    }
+   
+   
+    protected List<List<String>>  getCategories()
+    {
+        return categories; 
+    }
+    
+}
